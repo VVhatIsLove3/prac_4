3fa069841f1483c87d39086f0ffad3a5e31a4566
VVhatIsLove3 <153982647+VVhatIsLove3@users.noreply.github.com>
2025-04-18 13:24:04 +0300

    Update client.c

--- client.c
+++ client.c
@@ -5,66 +5,93 @@
 #include <arpa/inet.h>
 
 #define BUFFER_SIZE 1024
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        fprintf(stderr, "Использование: %s <адрес сервера> <порт>\n", argv[0]);
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Использование: %s <IP сервера> <порт>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    const char *server_ip = argv[1];
+    const char* server_ip = argv[1];
     int port = atoi(argv[2]);
 
+    // Создание сокета
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0)
-    {
-        perror("Ошибка при открытии сокета");
+    if (client_socket < 0) {
+        perror("Ошибка создания сокета");
         return 1;
     }
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port);
-    inet_pton(AF_INET, server_ip, &server_address.sin_addr);
-        // Подключаемся к серверу
-    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-    {
-        perror("Ошибка при подключении к серверу");
+
+    // Настройка адреса сервера
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+    
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("Ошибка в IP адресе");
+        close(client_socket);
         return 1;
     }
-    else
-    {
-        connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+
+    // Подключение к серверу
+    printf("Подключаемся к серверу %s:%d...\n", server_ip, port);
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Ошибка подключения");
+        close(client_socket);
+        return 1;
     }
+    printf("Успешно подключено!\n\n");
 
     int low = 1;
     int high = 1000;
-    int chislo;
+    int guess;
     char buffer[BUFFER_SIZE];
-    int count_of_attempt = 0;
+    int attempt = 0;
 
-    while (low <= high)
-    {
-        chislo = (low + high) / 2;  // Используем бинарный поиск
-        count_of_attempt++;
-        printf("Попытка %d: Угадываю число %d\n", count_of_attempt, chislo);
-        snprintf(buffer, sizeof(buffer), "%d", chislo);
-        send(client_socket, buffer, strlen(buffer), 0);
+    printf("Начинаем угадывать число от %d до %d\n", low, high);
 
-        int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
-        buffer[bytes_received] = '\0';
-        printf("Сервер: %s\n", buffer);
-        if (strstr(buffer, "Поздравляем!") != NULL)
+    while (low <= high) {
+        attempt++;
+        guess = (low + high) / 2;
+        
+        // Формируем и отправляем предположение
+        printf("Попытка %d: отправляем число %d\n", attempt, guess);
+        snprintf(buffer, sizeof(buffer), "%d", guess);
+        
+        if (send(client_socket, buffer, strlen(buffer), 0) < 0) {
+            perror("Ошибка отправки");
             break;
-        else
-        {
-            if(strstr(buffer, "Загаданное число больше")!=NULL)
-                low=chislo+1;
-            if (strstr(buffer, "Загаданное число меньше")!=NULL)
-                high=chislo-1;
         }
 
+        // Получаем ответ сервера
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            perror("Ошибка получения данных или сервер закрыл соединение");
+            break;
+        }
+        buffer[bytes_received] = '\0';
+        
+        // Анализируем ответ
+        printf("Ответ сервера: %s\n", buffer);
+        
+        if (strstr(buffer, "Поздравляем")) {
+            printf("\nУспех! Число угадано за %d попыток\n", attempt);
+            break;
+        } else if (strstr(buffer, "Больше")) {
+            printf("Загаданное число больше %d\n", guess);
+            low = guess + 1;
+        } else if (strstr(buffer, "Меньше")) {
+            printf("Загаданное число меньше %d\n", guess);
+            high = guess - 1;
+        } else {
+            printf("Неизвестный ответ сервера\n");
+        }
+        
+        printf("Новый диапазон поиска: %d - %d\n\n", low, high);
     }
+
     close(client_socket);
+    printf("Соединение закрыто\n");
     return 0;
 }
